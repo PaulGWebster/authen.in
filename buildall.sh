5fb20f4cb16fbdb59c6f18b901692362bb5d2dac
Paul G Webster <paul.g.webster@googlemail.com>
2023-01-03 02:33:06 +0000

    Clean build success

---
+++ buildall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Creating full image set!"
+
+echo "= Stage0" && cd images/stage0 && ./build.sh   \
+&& echo "= Stage1" && cd ../stage1 && ./build.sh    \
+&& echo "= Stage2" && cd ../stage2 && ./build.sh    \
+&& echo "Core image: authen:base is ready for use"
